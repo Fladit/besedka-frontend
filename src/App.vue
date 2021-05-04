4f9ed189e5d8f7cfca163451696ab9fa878b5704
Fladit <zatsarin77@gmail.com>
2021-05-04 23:39:04 +0300

    add login component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Login/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Login from "@/components/TheLoginWindow";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TheLoginWindow.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="login">
+    <input class="login_input" id="usernameInput" v-model="username" placeholder="Введите логин или email">
+    <input class="login_input" id="passwordInput" v-model="password" placeholder="Введите пароль">
+    <button class="login_button" @click="login">Войти</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data: function () {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    async login() {
+      console.log("Login")
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 24px;
+}
+
+.login_input {
+  width: 300px;
+  height: 60px;
+  border-radius: 15px;
+  margin-bottom: 20px;
+  font-size: 20px;
+  padding: 10px 15px;
+}
+
+.login_button {
+  width: 100px;
+  height: 40px;
+}
+</style>
